b361d97a3ecba74f1bbb29892d2589a0a0541d83
Douglas Santana <83181628+douglas-santana-dev@users.noreply.github.com>
2026-05-24 09:14:11 -0300

    Primeiro commit do Portal Pixel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Article from "./pages/Article";
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
 
         <Route path="/" element={<Home />} />
@@ -25,7 +25,7 @@ function App() {
         <Route path="/buscar" element={<SearchPage />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
